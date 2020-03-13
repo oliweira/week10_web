5039891813c6bfef4f79797c5a85f4d2d24c8972
Oliweira <oliweira@gmail.com>
2020-03-12 23:53:22 -0300

    Adicionando um compontente para devitem

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from 'react';
 import api from './services/api';
 
 import './Global.css';
-import './App.css'
-import './Sidebar.css'
-import './Main.css'
+import './App.css';
+import './Sidebar.css';
+import './Main.css';
+
+import DevItem from './components/DevItem';
 
 // Compopnente: Bloco isolado de HTML, CSS e JS,o qual não interfere no restante da aplicação
 // Propriedade: Informações que um componente PAI passa para o componente FILHO
@@ -12,9 +14,9 @@ import './Main.css'
 
 function App () {
 
+    const [devs,setDevs] = useState ([]);
     const [github_username, setGithubUsername] = useState ('');
     const [techs, setTechs] = useState ('');
-
     const [latitude, setLatitude] = useState ('');
     const [longitude, setLongitude] = useState ('');
 
@@ -35,8 +37,17 @@ function App () {
         );
     }, []);
 
+    useEffect(() => {
+        async function loadDevs() {
+            const response = await api.get('/devs');
+            setDevs(response.data);
+        }
+
+        loadDevs();
+    }, []);
+
     async function handleAddDev(e) {
-        e.preventDefaul();
+        e.preventDefault();
 
         const response = await api.post('/devs', {
             github_username,
@@ -45,14 +56,17 @@ function App () {
             longitude,
         })
 
-        console.log(response.data);
+        setGithubUsername('');
+        setTechs('');
+
+        setDevs([...devs, response.data])
     }
 
     return (
         <div id="app">
             <aside>
                 <strong>Cadastrar</strong>
-                <form>
+                <form onSubmit={handleAddDev}>
                     <div className="input-block">
                         <label htmlFor="github_username">Usuário do Github</label>
                         <input name="github_username" id="github_username" required  value={github_username} 
@@ -88,50 +102,9 @@ function App () {
             </aside>
             <main>
                 <ul>
-                    <li className="dev-item">
-                        <header>
-                            <img src="https://avatars2.githubusercontent.com/u/1918147?s=400&u=3fce39c0f932ae979e2280940ff50dc811c7ff5c&v=4" alt="" />
-                            <div className="user-info">
-                                <strong>Rafael Leite</strong>
-                                <span>ReactJs, React Native, Node.js</span>
-                            </div>
-                        </header>
-                        <p>Blá bla bla bla bla bla</p>
-                        <a href="https://github.com/oliweira">Acessar perfil no Github</a>
-                    </li>
-                    <li className="dev-item">
-                        <header>
-                            <img src="https://avatars2.githubusercontent.com/u/1918147?s=400&u=3fce39c0f932ae979e2280940ff50dc811c7ff5c&v=4" alt="" />
-                            <div className="user-info">
-                                <strong>Rafael Leite</strong>
-                                <span>ReactJs, React Native, Node.js</span>
-                            </div>
-                        </header>
-                        <p>Blá bla bla bla bla bla</p>
-                        <a href="https://github.com/oliweira">Acessar perfil no Github</a>
-                    </li>
-                    <li className="dev-item">
-                        <header>
-                            <img src="https://avatars2.githubusercontent.com/u/1918147?s=400&u=3fce39c0f932ae979e2280940ff50dc811c7ff5c&v=4" alt="" />
-                            <div className="user-info">
-                                <strong>Rafael Leite</strong>
-                                <span>ReactJs, React Native, Node.js</span>
-                            </div>
-                        </header>
-                        <p>Blá bla bla bla bla bla</p>
-                        <a href="https://github.com/oliweira">Acessar perfil no Github</a>
-                    </li>
-                    <li className="dev-item">
-                        <header>
-                            <img src="https://avatars2.githubusercontent.com/u/1918147?s=400&u=3fce39c0f932ae979e2280940ff50dc811c7ff5c&v=4" alt="" />
-                            <div className="user-info">
-                                <strong>Rafael Leite</strong>
-                                <span>ReactJs, React Native, Node.js</span>
-                            </div>
-                        </header>
-                        <p>Blá bla bla bla bla bla</p>
-                        <a href="https://github.com/oliweira">Acessar perfil no Github</a>
-                    </li>
+                    {devs.map(dev => (
+                        <DevItem key={dev._id} dev={dev} />
+                    ))}
                 </ul>                                
             </main>
         </div>
